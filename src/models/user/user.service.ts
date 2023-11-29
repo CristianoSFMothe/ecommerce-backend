8502f5b54c0381419a73bdc13ff8ee4081dff171
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-11-28 21:49:01 -0300

    feat: create user service

--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,4 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+
+    const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+    const user: User = {
+      ...createUserDto,
+      id: this.users.length + 1,
+      password: passwordHashed,
+    };
+
+    this.users.push(user);
+
+    console.log('passwordHashed', passwordHashed);
+
+    return user;
+  }
+
+  public async getAllUsers(): Promise<User[]> {
+    return this.users;
+  }
+}

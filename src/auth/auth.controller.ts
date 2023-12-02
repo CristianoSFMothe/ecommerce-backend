d13fbd7bdb588cdbc24ab8971155127fa7d05ee5
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-12-02 16:30:21 -0300

    feat: create token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ReturnUserDto } from 'src/models/user/dtos/returnUser.dto';
 import { LoginDto } from './dtos/login.dto';
 import { AuthService } from './auth.service';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post()
-  public async login(@Body() loginDto: LoginDto): Promise<ReturnUserDto> {
-    return new ReturnUserDto(await this.authService.login(loginDto));
+  public async login(@Body() loginDto: LoginDto): Promise<ReturnLogin> {
+    return this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserEntity } from '../models/user/entities/user.entity';
 import { LoginDto } from './dtos/login.dto';
-import { UserService } from 'src/models/user/user.service';
+import { UserService } from '../models/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/models/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  public async login(loginDto: LoginDto): Promise<UserEntity> {
+  public async login(loginDto: LoginDto): Promise<ReturnLogin> {
     const user: UserEntity | undefined = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
@@ -19,6 +26,9 @@ export class AuthService {
       throw new NotFoundException('E-mail or password invalid');
     }
 
-    return user;
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
   }
 }

c066922874e133eee9576476dd71f4ffb9bb528e
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-12-10 23:35:40 -0300

    feat: unit test in cart product

--- src/models/cart-product/__test__/cart-product.service.spec.ts
+++ src/models/cart-product/__test__/cart-product.service.spec.ts
@@ -1,18 +1,111 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CartProductService } from '../cart-product.service';
+import { productMock } from '../../../models/product/__mocks__/product.mock';
+import { cartMock } from '../../../models/cart/__mocks__/cart.mock';
+import { NotFoundException } from '@nestjs/common';
+import { cartProductMock } from '../__mocks___/cart-product.mock';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ProductService } from '../../../models/product/product.service';
+import { Repository } from 'typeorm';
+import { CartProductEntity } from '../entities/cart-product.entity';
+import { returnDeleteMock } from '../../../__mocks__/return-delete.mock';
+import { insertCartMock } from '../../../models/cart/__mocks__/insert-cart.mock';
 
 describe('CartProductService', () => {
   let service: CartProductService;
+  let prodcutService: ProductService;
+  let cartProductRepository: Repository<CartProductEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CartProductService],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: {
+            findProductById: jest.fn().mockResolvedValue(productMock),
+          },
+        },
+        {
+          provide: getRepositoryToken(CartProductEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(cartProductMock),
+            save: jest.fn().mockResolvedValue(cartProductMock),
+            delete: jest.fn().mockResolvedValue(returnDeleteMock),
+          },
+        },
+        CartProductService,
+      ],
     }).compile();
 
     service = module.get<CartProductService>(CartProductService);
+    prodcutService = module.get<ProductService>(ProductService);
+    cartProductRepository = module.get<Repository<CartProductEntity>>(
+      getRepositoryToken(CartProductEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(prodcutService).toBeDefined();
+    expect(cartProductRepository).toBeDefined();
+  });
+
+  it('should return Delete Result after delete product', async () => {
+    const deleteResult = await service.deleteProductCart(
+      productMock.id,
+      cartMock.id,
+    );
+
+    expect(deleteResult).toEqual(returnDeleteMock);
+  });
+
+  it('should return error in exception delete', async () => {
+    jest.spyOn(cartProductRepository, 'delete').mockRejectedValue(new Error());
+
+    expect(
+      service.deleteProductCart(productMock.id, cartMock.id),
+    ).rejects.toThrowError();
+  });
+
+  it('should return CartProduct after create', async () => {
+    const productCart = await service.createProductInCart(
+      insertCartMock,
+      cartMock.id,
+    );
+
+    expect(productCart).toEqual(cartProductMock);
+  });
+
+  it('should return error in exception delete', async () => {
+    jest.spyOn(cartProductRepository, 'save').mockRejectedValue(new Error());
+
+    expect(
+      service.createProductInCart(insertCartMock, cartMock.id),
+    ).rejects.toThrowError();
+  });
+
+  it('should return CartProduct if exist', async () => {
+    const productCart = await service.verifyProductInCart(
+      productMock.id,
+      cartMock.id,
+    );
+
+    expect(productCart).toEqual(cartProductMock);
+  });
+
+  it('should return error if not found', async () => {
+    jest.spyOn(cartProductRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(
+      service.verifyProductInCart(productMock.id, cartMock.id),
+    ).rejects.toThrowError(NotFoundException);
+  });
+
+  it('should return error in exception verifyProductInCart', async () => {
+    jest.spyOn(cartProductRepository, 'findOne').mockRejectedValue(new Error());
+
+    expect(
+      service.verifyProductInCart(productMock.id, cartMock.id),
+    ).rejects.toThrowError(Error);
   });
 });

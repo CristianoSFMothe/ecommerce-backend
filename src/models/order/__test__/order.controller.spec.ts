42af8a207d116ca5275d0de89c22f74a59edcdf9
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-12-14 00:06:17 -0300

    feat: insert unit test in findAllOrders

--- src/models/order/__test__/order.controller.spec.ts
+++ src/models/order/__test__/order.controller.spec.ts
@@ -16,6 +16,7 @@ describe('OrderController', () => {
           useValue: {
             createOrder: jest.fn().mockResolvedValue(orderMock),
             findOrdersByUserId: jest.fn().mockResolvedValue([orderMock]),
+            findAllOrders: jest.fn().mockResolvedValue([orderMock]),
           },
         },
       ],
@@ -30,9 +31,23 @@ describe('OrderController', () => {
     expect(controller).toBeDefined();
     expect(orderService).toBeDefined();
   });
+
   it('should return orders in findOrdersByUserId', async () => {
     const orders = await controller.findOrdersByUserId(userEntityMock.id);
 
     expect(orders).toEqual([orderMock]);
+  });
+
+  it('should return orders in findAllOrders', async () => {
+    const spy = jest.spyOn(orderService, 'findAllOrders');
+    const orders = await controller.findAllOrders();
+
+    expect(orders).toEqual([
+      {
+        id: orderMock.id,
+        date: orderMock.date.toString(),
+      },
+    ]);
+    expect(spy.mock.calls.length).toEqual(1);
   });
 });

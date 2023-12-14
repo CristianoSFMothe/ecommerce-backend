f0315c4cd9dda3956fcb6a10a0e7ee4d7ba06bdb
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-12-13 21:15:53 -0300

    feat: create functions getInfoUser

--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -5,6 +5,8 @@ import { UserEntity } from './entities/user.entity';
 import { ReturnUserDto } from './dtos/returnUser.dto';
 import { UpdatedPasswordDto } from './dtos/update-password.dto';
 import { UserId } from '../../decorators/user-id.decorator';
+import { UserType } from './enum/user-type.enum';
+import { Roles } from '../../decorators/roles.decorator';
 
 @Controller('user')
 export class UserController {
@@ -17,13 +19,15 @@ export class UserController {
     return this.userService.createUser(createUserDto);
   }
 
-  @Get()
+  @Roles(UserType.Admin)
+  @Get('/all')
   public async getAllUsers(): Promise<ReturnUserDto[]> {
     return (await this.userService.getAllUsers()).map(
       (useEntity) => new ReturnUserDto(useEntity),
     );
   }
 
+  @Roles(UserType.Admin)
   @Get('/:userId')
   public async getUserById(
     @Param('userId') userId: number,
@@ -33,11 +37,20 @@ export class UserController {
     );
   }
 
+  @Roles(UserType.Admin, UserType.User)
   @Patch()
   public async updatedPasswordUser(
     @Body() updatedPasswordDto: UpdatedPasswordDto,
     @UserId() userId: number,
   ): Promise<UserEntity> {
     return this.userService.updatedPasswordUser(updatedPasswordDto, userId);
+  }
+
+  @Roles(UserType.Admin, UserType.User)
+  @Get()
+  public async getInfoUser(@UserId() userId: number): Promise<ReturnUserDto> {
+    return new ReturnUserDto(
+      await this.userService.getUserByIdUsingRelations(userId),
+    );
   }
 }

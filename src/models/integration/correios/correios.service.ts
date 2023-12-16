6f6f531e38fc7373dfc2bcc0d9014c818fe2366e
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-12-15 22:24:12 -0300

    feat: find city and state by cep

--- src/models/integration/correios/correios.service.ts
+++ src/models/integration/correios/correios.service.ts
@@ -4,17 +4,22 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
+import { ReturnCepExternalDto } from './dtos/return-cep-external.dto';
+import { CityService } from 'src/models/city/city.service';
 
 @Injectable()
 export class CorreiosService {
   URL_CORREIOS = process.env.URL_CEP_CORREIOS;
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly cityService: CityService,
+  ) {}
 
-  public async findAddressByCep(cep: string): Promise<AxiosResponse<any>> {
-    return this.httpService.axiosRef
-      .get(this.URL_CORREIOS.replace('{CEP}', cep))
+  public async findAddressByCep(cep: string): Promise<ReturnCepExternalDto> {
+    const retunrCep: ReturnCepExternalDto = await this.httpService.axiosRef
+      .get<ReturnCepExternalDto>(this.URL_CORREIOS.replace('{CEP}', cep))
       .then((result) => {
         if (result.data.erro === 'true') {
           throw new NotFoundException('CEP not found.');
@@ -26,5 +31,12 @@ export class CorreiosService {
           `Error in connection request API Correios. ${error.message}`,
         );
       });
+
+    const city = await this.cityService.findCityByName(
+      retunrCep.localidade,
+      retunrCep.uf,
+    );
+
+    return retunrCep;
   }
 }

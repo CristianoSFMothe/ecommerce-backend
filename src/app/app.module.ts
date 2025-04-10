06d54486c5039e5798e9601297667ea8e7fe4ca2
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2025-04-10 00:24:22 -0300

    feat: add docker-compose and connection database Postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/models/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
